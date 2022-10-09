53766c0ff1024396bee8ba4b918e66b49c56b819
BaekMinHyeon <baek991013@naver.com>
2022-10-09 09:05:39 +0900

    보내는 메소드(Send, SendARP, SendGARP) 및 4바이트에서 int전환 메소드 추가

--- ChattingFileTransfer_2022/chatfile/IPLayer.java
+++ ChattingFileTransfer_2022/chatfile/IPLayer.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import EthernetLayer._ETHERNET_Frame;
+
 public class IPLayer implements BaseLayer {
 	public int nUnderLayerCount = 0;
 	public int nUpperLayerCount = 0;
@@ -59,7 +61,64 @@ public class IPLayer implements BaseLayer {
     	}
     }
     
-
+    public boolean Send(byte[] input, int length){
+    	int fragment = input[13] & 0xff; // TCP의 flag를 확인
+    	int sequence = byte4ToInt(input[4], input[5], input[6], input[7]); // TCP의 seq을 확인
+    	if(fragment == 0){ // 단편화 없음
+    		setHeader(length, 0, 0, sequence * offset);
+    	}
+    	else{ // 단편화
+    		if(fragment == 1) // 처음~마지막 전 데이터
+    			setHeader(length, 1, 1, sequence * offset);
+    		else // fragment == 2 마지막 데이터
+    			setHeader(length, 1, 1, sequence * offset);
+    	}
+    	byte[] data = ObjToByte(m_sHeader, input, length);
+    	EthernetLayer ethernetLayer = (EthernetLayer)this.GetUnderLayer(1); // 0일지 1일지 Dlg를 만들지 않아 미정
+    	ethernetLayer.Send(data, data.length);
+    	return true;
+    }
+    
+    public boolean SendARP(byte[] dstAddr){
+    	ARPLayer arpLayer = (ARPLayer)this.GetUnderLayer(0); // 0일지 1일지 Dlg를 만들지 않아 미정
+    	arpLayer.; // 메소드 이름 몰라서 아직 안씀
+    	return true;
+    }
+    
+    public boolean SendGARP(byte[] srcMac){
+    	ARPLayer arpLayer = (ARPLayer)this.GetUnderLayer(0); // 0일지 1일지 Dㅣg를 만들지 않아 미정
+    	arpLayer.; // 메소드 이름 몰라서 아직 안씀
+    	return true;
+    }
+    
+    private void setHeader(int length, int frag, int more, int fragoff){
+    	this.m_sHeader.ip_len = intToByte2(length); // tcp에서 전송되는 데이터 최대 1480이라서 2바이트 변환이면 충분
+    	// frag: 0(단편화 없음), 1(단편화 됨)  more: 0(마지막 데이터), 1(아직 데이터 남음)
+    	// ip_fragoff 16비트 중 맨 앞은 0 1비트 frag 1비트 more 13비트 fragoff
+    	this.m_sHeader.ip_fragoff[0] = (byte)(((frag & 0x1) << 6) | ((more & 0x1) << 5) | ((fragoff & 0x1f00) >> 8));
+    	this.m_sHeader.ip_fragoff[1] = (byte)(fragoff & 0xff);  	
+    }
+    
+    private byte[] ObjToByte(_IP Header, byte[] input, int length) {//data에 헤더 붙여주기
+		byte[] buf = new byte[length + Header_Size];
+		buf[0] = Header.ip_verlen;
+		buf[1] = Header.ip_tos;
+		for(int i = 0; i < 2; i++){
+			buf[2 + i] = Header.ip_len[i];
+			buf[4 + i] = Header.ip_id[i];
+			buf[6 + i] = Header.ip_fragoff[i];
+			buf[10 + i] = Header.ip_cksum[i];
+		}
+		buf[8] = Header.ip_ttl;
+		buf[9] = Header.ip_proto;
+		for(int i = 0; i < 4; i++){
+			buf[12 + i] = Header.ip_src.addr[i];
+			buf[16 + i] = Header.ip_dst.addr[i];
+		}
+		for (int i = 0; i < length; i++)
+			buf[Header_Size + i] = input[i];
+		return buf;
+	}
     
     private byte[] intToByte2(int value) {
         byte[] temp = new byte[2];
@@ -71,6 +130,10 @@ public class IPLayer implements BaseLayer {
 
     private int byte2ToInt(byte value1, byte value2) {
         return (int)(((value1 & 0xff) << 8) | (value2 & 0xff));
+    }
+    
+    private int byte4ToInt(byte value1, byte value2, byte value3, byte value4){
+    	return (int)(((value1 & 0xff) << 24) | ((value2 & 0xff) << 16) | ((value3 & 0xff) << 8) | (value4 & 0xff));
     }
     
     public void SetIPSrcAddress(byte[] srcAddress){
